e60a0820b010f70dcdf2f93d5e55290eb73f9e41
MarcusMultini <marcusmultini@gmail.com>
2025-11-24 21:05:29 -0300

    Nova Interface admin com build

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // 🌐 Arquivos do SITE
-                'resources/css/site.css',
-                'resources/js/site.js',
-
-                // 🛠️ Arquivos do ADMIN
-                'resources/css/admin.css',
-                'resources/js/admin.js',
-
-                // (opcional) app.css/app.js caso queira usar
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
